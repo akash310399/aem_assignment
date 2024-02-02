ab29f42cd5b070c298975b9e02ce4875bf9b9e29
akash <akash@gmIL.COM>
2024-02-03 00:13:34 +0530

    Question 6:       1. Got the product List from API       2. Sorted according to author

--- training-project/core/src/main/java/com/training/aem/core/models/impl/ProductCarousalModelImpl.java
+++ training-project/core/src/main/java/com/training/aem/core/models/impl/ProductCarousalModelImpl.java
@@ -3,10 +3,14 @@ package com.training.aem.core.models.impl;
 import com.training.aem.core.entities.ProductEntity;
 import com.training.aem.core.models.ProductCarousalModel;
 import com.training.aem.core.services.ProductDetailService;
+import com.training.aem.core.utils.CommonUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -22,9 +26,17 @@ public class ProductCarousalModelImpl implements ProductCarousalModel {
 
     private static final String BASE_URL = "https://fakestoreapi.com/products/";
 
+
+    //------Sort By--------
+
+    @ValueMapValue
+    @Default(values = StringUtils.EMPTY)
+    String sortFiltersType;
+
     List<ProductEntity> productEntityList = new ArrayList<>();
 
-    public List<ProductEntity> getProductEntityList(){
+    public List<ProductEntity> getProductEntityList() {
+        CommonUtils.sortProductEntitiesByPrice(productEntityList, sortFiltersType);
         return productEntityList;
     }
 
