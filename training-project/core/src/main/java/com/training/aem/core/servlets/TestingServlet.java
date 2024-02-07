f4e4dc9bb204ffa3cd2825c8994c3b22189509d8
akash <akash@gmIL.COM>
2024-02-07 13:24:07 +0530

    Create a workflow that gets triggered when the excel sheet is uploaded to a specific dam folder, then it should parse the values in the rows related to a product and then store it in the node structure.

--- training-project/core/src/main/java/com/training/aem/core/servlets/TestingServlet.java
+++ training-project/core/src/main/java/com/training/aem/core/servlets/TestingServlet.java
@@ -2,8 +2,12 @@ package com.training.aem.core.servlets;
 
 import com.training.aem.core.entities.ProductEntity;
 import com.training.aem.core.services.PageService;
+import com.training.aem.core.services.ReadExcelDataService;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.osgi.service.component.annotations.Component;
@@ -12,6 +16,8 @@ import org.osgi.service.component.annotations.Reference;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component(service = {Servlet.class},
         property = {
@@ -23,14 +29,35 @@ public class TestingServlet extends SlingSafeMethodsServlet {
     @Reference
     PageService pageService;
 
+    @Reference
+    ReadExcelDataService readExcelDataService;
+
+    @Reference
+    ResourceResolverFactory resourceResolverFactory;
+
     @Override
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
 
-        ProductEntity productEntity = new ProductEntity();
-        productEntity.setTitle("Name");
+//        ProductEntity productEntity = new ProductEntity();
+//        productEntity.setTitle("Name");
+//
+//        pageService.createPage(productEntity);
+//
+//        response.getWriter().write("servlet called.....");
 
-        pageService.createPage(productEntity);
+        try {
+            ResourceResolver resourceResolver = getResourceResolver();
+            readExcelDataService.getDataFromExcel(resourceResolver,"/content/dam/training/mobileSheet.xlsx");
+        } catch (LoginException e) {
+            throw new RuntimeException(e);
+        }
 
-        response.getWriter().write("servlet called.....");
+
+    }
+
+    private ResourceResolver getResourceResolver() throws LoginException {
+        Map<String, Object> map = new HashMap<>();
+        map.put(ResourceResolverFactory.SUBSERVICE, "useruser");
+        return resourceResolverFactory.getServiceResourceResolver(map);
     }
 }
