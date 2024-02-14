a3e3e6358da9d6a76cb8205724a712e69df3a649
akash <akash@gmIL.COM>
2024-02-14 13:33:04 +0530

    Added Email Service to it

--- training-project/core/src/main/java/com/training/aem/core/services/impl/PageServiceImpl.java
+++ training-project/core/src/main/java/com/training/aem/core/services/impl/PageServiceImpl.java
@@ -1,5 +1,10 @@
 package com.training.aem.core.services.impl;
 
+import com.day.cq.mailer.MailingService;
+import com.day.cq.mailer.MessageGateway;
+import com.day.cq.mailer.MessageGatewayService;
+import com.day.cq.notification.api.Notification;
+import com.day.cq.notification.api.NotificationService;
 import com.day.cq.replication.ReplicationActionType;
 import com.day.cq.replication.ReplicationException;
 import com.day.cq.replication.Replicator;
@@ -8,6 +13,8 @@ import com.day.cq.wcm.api.PageManager;
 import com.day.cq.wcm.api.WCMException;
 import com.training.aem.core.entities.ProductEntity;
 import com.training.aem.core.services.PageService;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.HtmlEmail;
 import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
@@ -16,6 +23,7 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import javax.jcr.Session;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,7 +38,15 @@ public class PageServiceImpl implements PageService {
     @Reference
     Replicator replicator;
 
+    @Reference
+    MessageGatewayService messageGatewayService;
 
+    private static final String RECIPIENT_MAIL = "akash31aks@gmail.com";
+    private static final String SERVICE_USER = "useruser";
+
+
+    @Reference
+    NotificationService notificationService;
 
     private static final String PARENT_PAGE_PATH = "/content/training-project/us";
 
@@ -49,6 +65,7 @@ public class PageServiceImpl implements PageService {
                     productEntity.getTitle(),PAGE_TEMPLATE,
                     productEntity.getTitle(),true);
             replicatePage(session,createdPage);
+            sendMailToAdmin();
         } catch (WCMException | LoginException e) {
             throw new RuntimeException(e);
         }
@@ -58,7 +75,7 @@ public class PageServiceImpl implements PageService {
 
     private ResourceResolver getResourceResolver() throws LoginException {
         Map<String, Object> map = new HashMap<>();
-        map.put(ResourceResolverFactory.SUBSERVICE, "useruser");
+        map.put(ResourceResolverFactory.SUBSERVICE, SERVICE_USER);
         return resourceResolverFactory.getServiceResourceResolver(map);
     }
 
@@ -68,6 +85,32 @@ public class PageServiceImpl implements PageService {
         } catch (ReplicationException e) {
             throw new RuntimeException("Failed to replicate the page", e);
         }
+    }
+
+
+
+    private void sendMailToAdmin(){
+        try {
+            MessageGateway<HtmlEmail> messageGateway = messageGatewayService.getGateway(HtmlEmail.class);
+
+            Date date = new Date();
+
+            HtmlEmail email = new HtmlEmail();
+            email.addTo(RECIPIENT_MAIL);
+            email.setSubject("Page Created By Scheduler at "+ date.getTime() );
+            email.setTextMsg("A new page has been created by"
+                    + SERVICE_USER + "under"
+                    + PARENT_PAGE_PATH);
+
+            messageGateway.send(email);
+
+        } catch (EmailException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void notificationTOAdmin(){
+
     }
 
 }
